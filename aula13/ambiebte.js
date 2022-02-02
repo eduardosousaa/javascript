825ccaa7446bc3a08648fbd78e2bc6fa956248d3
Eduardo de Sousa <97894971+eduardosousaa@users.noreply.github.com>
2022-02-02 10:13:58 -0300

    Repetições em JS

---
+++ aula13/ambiebte.js
@@ -0,0 +1,32 @@
+for (var c=1; c<=6; c++){
+    console.log(`Teste ${c}`)
+}
+
+
+
+
+/*
+var e = 1
+do {
+    console.log(`Passo ${e}`)
+    e++
+} while(e <= 6)
+*/
+
+
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ 
+}
+*/
+
+
+/*
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+*/
