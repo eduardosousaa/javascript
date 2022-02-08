c3598623d4d22dbb87f450c57b65d6ff6a4972e7
Eduardo de Sousa <97894971+eduardosousaa@users.noreply.github.com>
2022-02-08 09:07:42 -0300

    Fim do desafio

--- ex-parte3/ex-018/script.js
+++ ex-parte3/ex-018/script.js
@@ -26,6 +26,7 @@ function adicionar(){
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valores Inválidos ou já encontrado na lista')
     }
@@ -34,5 +35,29 @@ function adicionar(){
 }
 
 function finalizar() {
-    if(valores)
+    if(valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+
+        for(let pos in valores) {
+            soma += valores[pos]
+            if(valores[pos] > maior) 
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo, temos ${tot} valores cadastrados. </p>`
+        res.innerHTML += `<p> O maior valor informado é ${maior}. </p>`
+        res.innerHTML += `<p> O menor valor informado é ${menor}. </p>`
+        res.innerHTML += `<p> Somando todos os valores temos ${soma}. </p>`
+        res.innerHTML += `<p> A média dos valores digitados é ${Math.round(media)}. </p>`
+    }
 }
